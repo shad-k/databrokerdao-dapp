fc3b2f0c70d37b68e06c9af482ed832923100f1c
Rutger Bevers <rutger@conversationstarter.net>
2018-02-20 16:49:45 +0100

    map in landingpage

--- src/components/landing/LandingBackground.js
+++ src/components/landing/LandingBackground.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
 
-export default class LandingBackground extends Component {
+export default withScriptjs(withGoogleMap(class LandingBackground extends Component {
   render() {
     const StyledBackgroundDiv = styled.div`
       width: 100%;
@@ -10,13 +11,19 @@ export default class LandingBackground extends Component {
       position: absolute;
       top: 0;
       left: 0;
-      z-index: -1;
+      z-index: -10;
       background-color: rgb(100,180,190);
     `;
 
     return (
       <StyledBackgroundDiv>
+        <GoogleMap
+         defaultZoom={8}
+         defaultCenter={{ lat: -34.397, lng: 150.644 }}
+        >
+         {this.props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} />}
+        </GoogleMap>
       </StyledBackgroundDiv>
     );
   }
-}
+}))
