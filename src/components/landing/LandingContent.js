92ff4f9be58d5fc80ebbfd6d90f732007cccc184
Roderik van der Veer <roderik@settlemint.com>
2018-03-07 15:54:03 +0100

    fix: more case sensitive stuff

--- src/components/landing/LandingContent.js
+++ src/components/landing/LandingContent.js
@@ -1,63 +1,59 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import styled from 'styled-components';
-import { Card, CardText, Button, List, ListItemControl, FontIcon, Checkbox, Paper } from 'react-md';
-import { connect } from 'react-redux'
+import { Card, CardText, Button, Checkbox, Paper } from 'react-md';
+import { connect } from 'react-redux';
 
-import Icon from '../generic/Icon';
+import Icon from '../generic/icon';
 import { STREAMS_ACTIONS } from '../../redux/streams/actions';
 
 class LandingContent extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
 
     this.state = {
-      temperature:true,
-      humidity:true,
-      pm25:true,
-      pm10:true
+      temperature: true,
+      humidity: true,
+      pm25: true,
+      pm10: true
     };
   }
 
   onDiscoverButtonClicked = e => {
     //Update Redux filter
     const filters = [];
-    if(this.state.temperature)
-      filters.push('temperature');
-    if(this.state.humidity)
-      filters.push('humidity');
-    if(this.state.pm25)
-      filters.push('pm25');
-    if(this.state.pm10)
-      filters.push('pm10');
+    if (this.state.temperature) filters.push('temperature');
+    if (this.state.humidity) filters.push('humidity');
+    if (this.state.pm25) filters.push('pm25');
+    if (this.state.pm10) filters.push('pm10');
 
     this.props.updateFilter({
-      location:null,
-      types:filters
+      location: null,
+      types: filters
     });
 
     this.props.history.push('/discover');
-  }
+  };
 
   toggleFilterType(type) {
     const updatedState = this.state;
 
-    switch(type){
-      case 'temperature':{
-          updatedState.temperature = !this.state.temperature;
-          break;
+    switch (type) {
+      case 'temperature': {
+        updatedState.temperature = !this.state.temperature;
+        break;
       }
-      case 'humidity':{
-          updatedState.humidity = !this.state.humidity;
-          break;
+      case 'humidity': {
+        updatedState.humidity = !this.state.humidity;
+        break;
       }
-      case 'pm25':{
-          updatedState.pm25 = !this.state.pm25;
-          break;
+      case 'pm25': {
+        updatedState.pm25 = !this.state.pm25;
+        break;
       }
-      case 'pm10':{
-          updatedState.pm10 = !this.state.pm10;
-          break;
+      case 'pm10': {
+        updatedState.pm10 = !this.state.pm10;
+        break;
       }
     }
 
@@ -72,17 +68,17 @@ class LandingContent extends Component {
     `;
 
     const StyledH1 = styled.h1`
-      text-align:center;
-      font-weight:500;
-      color:rgba(0,0,0,0.7);
-      line-height:1.5;
-      margin-top:20px;
+      text-align: center;
+      font-weight: 500;
+      color: rgba(0, 0, 0, 0.7);
+      line-height: 1.5;
+      margin-top: 20px;
       margin-bottom: 10px;
     `;
 
     const StyledDiscoverButton = styled(Button)`
-      margin:0 auto;
-      display:block;
+      margin: 0 auto;
+      display: block;
     `;
 
     const StyledTypeButton = styled(Paper)`
@@ -90,78 +86,84 @@ class LandingContent extends Component {
       align-items: center;
       margin: 18px 10px 18px 10px;
       padding-left: 12px;
-    `
+    `;
 
     const StyledTypeButtonLabel = styled.span`
-      flex:1;
-      margin-left:12px;
-      line-height:1;
+      flex: 1;
+      margin-left: 12px;
+      line-height: 1;
     `;
 
     return (
       <div>
-        <StyledCard className='md-block-centered'>
+        <StyledCard className="md-block-centered">
           <CardText>
-            <StyledH1>Buy sensor data on the global market for sensor data</StyledH1>
-            <div style={{padding:"0 80px 20px 80px", display:"flex"}}>
-              <div style={{flex:"1"}}>
+            <StyledH1>
+              Buy sensor data on the global market for sensor data
+            </StyledH1>
+            <div style={{ padding: '0 80px 20px 80px', display: 'flex' }}>
+              <div style={{ flex: '1' }}>
                 <StyledTypeButton zDepth={1}>
-                  <Icon icon="temperature"/>
+                  <Icon icon="temperature" />
                   <StyledTypeButtonLabel>Temperature</StyledTypeButtonLabel>
                   <Checkbox
                     id="temperature"
                     value="temperature"
                     name="temperature"
                     aria-label="temperature"
-                    style={{display:"inline"}}
+                    style={{ display: 'inline' }}
                     checked={this.state.temperature}
                     onChange={() => this.toggleFilterType('temperature')}
                   />
                 </StyledTypeButton>
                 <StyledTypeButton zDepth={1}>
-                  <Icon icon="humidity"/>
+                  <Icon icon="humidity" />
                   <StyledTypeButtonLabel>Humidity</StyledTypeButtonLabel>
                   <Checkbox
                     id="humidity"
                     value="humidity"
                     name="humidity"
                     aria-label="humidity"
-                    style={{display:"inline"}}
+                    style={{ display: 'inline' }}
                     checked={this.state.humidity}
                     onChange={() => this.toggleFilterType('humidity')}
                   />
                 </StyledTypeButton>
               </div>
-              <div style={{flex:"1"}}>
+              <div style={{ flex: '1' }}>
                 <StyledTypeButton zDepth={1}>
-                  <Icon icon="pm25"/>
+                  <Icon icon="pm25" />
                   <StyledTypeButtonLabel>PM 2.5</StyledTypeButtonLabel>
                   <Checkbox
                     id="pm25"
                     value="pm25"
                     name="pm25"
                     aria-label="pm25"
-                    style={{display:"inline"}}
+                    style={{ display: 'inline' }}
                     checked={this.state.pm25}
                     onChange={() => this.toggleFilterType('pm25')}
                   />
                 </StyledTypeButton>
                 <StyledTypeButton zDepth={1}>
-                  <Icon icon="pm10"/>
+                  <Icon icon="pm10" />
                   <StyledTypeButtonLabel>PM 10</StyledTypeButtonLabel>
                   <Checkbox
                     id="pm10"
                     value="pm10"
                     name="pm10"
                     aria-label="pm10"
-                    style={{display:"inline"}}
+                    style={{ display: 'inline' }}
                     checked={this.state.pm10}
                     onChange={() => this.toggleFilterType('pm10')}
                   />
                 </StyledTypeButton>
               </div>
             </div>
-            <StyledDiscoverButton primary raised onClick={this.onDiscoverButtonClicked.bind(this)}>
+            <StyledDiscoverButton
+              primary
+              raised
+              onClick={this.onDiscoverButtonClicked.bind(this)}
+            >
               Discover sensors
             </StyledDiscoverButton>
           </CardText>
@@ -173,8 +175,8 @@ class LandingContent extends Component {
 
 function mapDispatchToProps(dispatch) {
   return {
-    updateFilter: (filter) => STREAMS_ACTIONS.updateFilter(dispatch, filter)
-  }
+    updateFilter: filter => STREAMS_ACTIONS.updateFilter(dispatch, filter)
+  };
 }
 
-export default connect(null, mapDispatchToProps)(withRouter(LandingContent))
+export default connect(null, mapDispatchToProps)(withRouter(LandingContent));
