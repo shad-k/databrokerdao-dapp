ec7d01208d27f3727bf95ac6094a23529d999758
Rutger Bevers <rutger@conversationstarter.net>
2018-03-15 15:02:23 +0100

    feat: front end clustering

---
+++ src/components/discover/Cluster.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { OverlayView } from "react-google-maps"
+import styled from 'styled-components';
+
+export default class Cluster extends Component {
+  render() {
+    const zIndex = 90000 - this.props.position.lat*1000 + 10;
+    const StyledContainer = styled.div`
+      transform:translate(-50%, -100%);
+      z-index: ${zIndex};
+      background-color: #2e3192;
+      width:40px;
+      height:40px;
+      border-radius:20px;
+      display:flex;
+      justify-content: center;
+      align-items: center;
+    `;
+
+    const StyledLabel = styled.span`
+      color:white;
+      font-weight:500;
+      font-size: 15px;
+    `;
+
+    return(
+      <OverlayView
+        position={{ lat: this.props.position.lat, lng: this.props.position.lng }}
+        mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}
+      >
+        <StyledContainer>
+          <StyledLabel>
+            {this.props.label}
+          </StyledLabel>
+        </StyledContainer>
+      </OverlayView>
+    );
+  }
+}
