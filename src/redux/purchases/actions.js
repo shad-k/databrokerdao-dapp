f89f6c3b3935ed85ffa40d8e170f4cd048c0eecc
Rutger Bevers <rutger@conversationstarter.net>
2018-03-22 17:09:42 +0100

    fix: getting token address and spender address

--- src/redux/purchases/actions.js
+++ src/redux/purchases/actions.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import axios from '../../utils/axios';
+import Bluebird from 'bluebird';
 
 export const PURCHASES_TYPES = {
   FETCH_PURCHASES: 'FETCH_PURCHASES',
@@ -11,42 +12,58 @@ export const PURCHASES_TYPES = {
 export const PURCHASES_ACTIONS = {
   fetchPurchases: () => {
     return (dispatch, getState) => {
+      const state = getState();
+
       dispatch({
         type: PURCHASES_TYPES.FETCHING_PURCHASES,
         value: true
       });
 
       const authenticatedAxiosClient = axios(null,true);
-      authenticatedAxiosClient.get(
-        "/purchaseregistry/list" //?purchaser=0x31401412f6902e0cd41822eeced276c80134e916
-      ).then(response => {
-        console.log("All purchases:");
-        console.log(response);
+      // authenticatedAxiosClient.get(
+      //   "/purchaseregistry/list" //?purchaser=0x31401412f6902e0cd41822eeced276c80134e916
+      // ).then(response => {
+      //   console.log("All purchases:");
+      //   console.log(response);
+      //
+      //   // dispatch({
+      //   //   type: WALLET_TYPES.FETCH_WALLET,
+      //   //   wallet
+      //   // });
+      // }).catch(error => {
+      //   console.log(error);
+      // });
 
-        // dispatch({
-        //   type: WALLET_TYPES.FETCH_WALLET,
-        //   wallet
-        // });
-      }).catch(error => {
-        console.log(error);
-      });
+      function getDtxTokenRegistry() {
+        console.log("1");
+        return authenticatedAxiosClient.get("/dtxtokenregistry/list");
+      }
 
+      function getPurchaseRegistry() {
+        console.log("2");
+        return authenticatedAxiosClient.get("/purchaseregistry/list");
+      }
 
+      //TODO test code to test making a purchase
+      
+      Bluebird.all([getDtxTokenRegistry(),getPurchaseRegistry()])
+        .then((responses) => {
+          const deployedTokenContractAddress = responses[0].data.items[0].contractaddress;
+          const spenderAddress = responses[1].data.base.key;
 
-      authenticatedAxiosClient.post("/purchaseregistry/purchaseaccess",{
-        stream:"0xe24e233958cc7286d214a241c56697699c495188",
-        endtime:"1534755600",
-        metadata:"blablahash"
-      }).then(response => {
-        console.log(response.debug.base.key);
+          // Now approve the tokens
+          authenticatedAxiosClient.post(`/dtxtoken/${deployedTokenContractAddress}/approve`,{
+            spender: spenderAddress, // The contract that will spend the tokens (some function of the contract will)
+            value: "100" //TODO
+          }).then(response => {
+            console.log(response);
+            //Tokens have been allocated - now we can make the purchase!
 
-        // dispatch({
-        //   type: WALLET_TYPES.FETCH_WALLET,
-        //   wallet
-        // });
-      }).catch(error => {
-        console.log(error);
-      });
+          }).catch(error => {
+            console.log(error);
+          });
+        })
+
 
       //TODO hear from Silke in what format purchases are supplied
       const parsedResponse = [
